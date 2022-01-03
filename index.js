f71de3912af664dad1664b86ff06ba7020e60ee3
Heeji Hyun <wlgmlgus@gmail.com>
2022-01-02 23:09:01 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(`${name}`);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(`${name}`);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const appendCat = [...cats, `Broom`]
+    return appendCat
+}
+
+function prependCat(name) {
+    const prependCat = ["Arnold", ...cats]
+    return prependCat
+}
+
+function removeLastCat(name) {
+    const removeLastCat = [cats]
+    cats.slice(1)
+}
+
+function removeFirstCat(name) {
+    cats.shift(`${name}`)
+}
